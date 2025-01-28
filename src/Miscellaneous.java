fb5dfe99932cda010be6169eaef76d923d08df94
Zephyros <93495076+Zephyrosickness@users.noreply.github.com>
2025-01-28 10:39:44 -0500

    remove bulk output (TEMP)

--- src/Miscellaneous.java
+++ src/Miscellaneous.java
@@ -45,37 +45,6 @@ public class Miscellaneous {
         };
     }
 
-    //outputs all possible results to outspeed at certain points (reduces clutter in main method)
-    public static void outputAllPossibleResults(int level, int targetStat, int targetBaseStat){
-        System.out.println("\n--[SPEED VALUES NEEDED TO OUTSPEED TARGET]--\n(All calculations assume 31 IVs)");
-
-
-
-        System.out.println("-[ONE BOOST]-");
-
-        //+1 boost no evs neutral
-        System.out.println("[NO EVS, NEUTRAL NATURE]: "+Calculators.findLeastStatBoosted(31,0,1,level,targetStat,targetBaseStat,1));
-        //+1 boost no evs positive
-        System.out.println("[NO EVS, POSITIVE NATURE]: "+Calculators.findLeastStatBoosted(31,0,1.1,level,targetStat,targetBaseStat,1));
-        //+1 boost 252 evs neutral
-        System.out.println("[252 EVS, NEUTRAL NATURE]: "+Calculators.findLeastStatBoosted(31,252/4,1,level,targetStat,targetBaseStat,1));
-        //+1 boost 252 evs positive
-        System.out.println("[252 EVS, POSITIVE NATURE]: "+Calculators.findLeastStatBoosted(31,252/4,1.1,level,targetStat,targetBaseStat,1));
-
-
-
-        System.out.println("\n-TWO BOOSTS]-");
-
-        //+2 boost no evs neutral
-        System.out.println("[NO EVS, NEUTRAL NATURE]: "+Calculators.findLeastStatBoosted(31,0,1,level,targetStat,targetBaseStat,2));
-        //+2 boost no evs positive
-        System.out.println("[NO EVS, POSITIVE NATURE]: "+Calculators.findLeastStatBoosted(31,0,1.1,level,targetStat,targetBaseStat,2));
-        //+2 boost 252 evs neutral
-        System.out.println("[252 EVS, NEUTRAL NATURE]: "+Calculators.findLeastStatBoosted(31,252/4,1,level,targetStat,targetBaseStat,2));
-        //+2 boost 252 evs positive
-        System.out.println("[252 EVS, POSITIVE NATURE]: "+Calculators.findLeastStatBoosted(31,252/4,1.1,level,targetStat,targetBaseStat,2));
-    }
-
     //get selected mon
     public static int[] getSelectedPokemon(){
         boolean keepGoing = true;
