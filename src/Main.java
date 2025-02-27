78dfc9533e54a9a941bcc96e886b398ef06129f5
Zephyros <zxphyros@gmail.com>
2025-02-27 18:30:24 -0500

    make the program selector

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         Database.initialize(); //loads the pokedex
-        //EVCalculatorUI.initUI(); //uncomment later
-        PokemonDatabaseUI.initUI();
 
-        /*to do
-        abilities
-         */
+        final int SIZE = 200;
+        final JFrame frame = new JFrame();
+        frame.setSize(SIZE, SIZE);
+        frame.setLayout(new GridLayout());
+        frame.setVisible(true);
+
+        final JButton ev = new JButton("EV Optimizer");
+        frame.add(ev);
+
+        final JButton db = new JButton("Pokemon Database");
+        frame.add(db);
+
+        db.addActionListener(_-> PokemonDatabaseUI.initUI());
+
+        ev.addActionListener(_-> {
+            try{EVCalculatorUI.initUI();
+            }catch(IOException e) {throw new RuntimeException(e);}
+        });
     }
 }
