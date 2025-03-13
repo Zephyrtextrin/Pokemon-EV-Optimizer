44517a4522da51ba2421dada4a4c31bc09f5209b
Zephyros <zxphyros@gmail.com>
2025-03-13 10:20:17 -0400

    SUPER UNFINISHED make new db xml instead of class
    unfinished

--- src/Constants.java
+++ src/Constants.java
@@ -6,6 +6,7 @@ public class Constants{
     public static final boolean DEBUG_DAMAGE_MODE = false; //enable/disable debug statements for damage calculations (these are quite lengthy so they get their own)
     public static final boolean DEBUG_CALC_MODE = false; //enable/disable debug statements for other calculations
     public static final boolean DEBUG_UI_MODE = false; //enable/disable debug statements for the UI.
+    public static final boolean DEBUG_DB_MODE = true; //used exclusively to test xml file reading
     public static final boolean DEBUG_DISABLE_OUTPUT = false; //if true: disables all output intended for end users
 
     public static final String[] BOOSTS = {"+0","-6","-5","-4","-3","-2","-1","+1","+2","+3","+4","+5","+6"};
